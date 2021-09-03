585bc8d629199cfac83fcab35ca6abf797aca15c
THIAGO VALE <77752211+thiago-vale@users.noreply.github.com>
2021-09-03 10:25:47 -0300

    Update pipeline.py

--- calculadora-de-imóveis/calculadora-de-alugueis-main/calculadora-de-alugueis-main/pipeline.py
+++ calculadora-de-imóveis/calculadora-de-alugueis-main/calculadora-de-alugueis-main/pipeline.py
@@ -1,9 +1,5 @@
 import os
 
-# Super simple pipeline chaining the crawler, cleaning,
-# removing outliers and training scripts
-
-# os.system("python crawler.py")
-os.system("python cleaning.py")
-os.system("python removing_outliers.py")
-os.system("python model_training.py")
+os.system("extrator_de_imoveis.py")
+os.system("Tratando_os_dados.py")
+os.system("Limpando outliers.py")
